9ad8d4552c9ecd503120379ef191d2d5845a9528
NemVaar <63074163+NemVaar@users.noreply.github.com>
2020-04-02 17:07:11 -0400

    Create schedule.cpp
    Implementation file for schedule class

---
+++ schedule.cpp
@@ -0,0 +1,56 @@
+
+//schedule.cpp
+
+//Definition file for schedule class
+
+//include necessary directories
+#include "schedule.h"
+
+//set namespace to std
+using namespace std;
+
+//constructor
+schedule::Schedule() {
+	
+}
+
+//printAppt function
+//Displays appointment
+void schedule::printAppt() {
+	
+}
+
+//get and set functions
+string schedule::getDate() {
+	
+}
+string schedule::getTimeslot() {
+	
+}
+ksuPatient schedule::getPatient() {
+	
+}
+//getStaff();
+
+void schedule::setDate() {
+	
+}
+void schedule::setTimeslot(string timeslot) {
+	
+}
+void schedule::setPatient(ksuPatient patient) {
+	
+}
+//void setStaff();
+
+//saveAppt function
+//saves appointment to schedule
+void schedule::saveAppt(appointment appt) {
+	
+}
+
+//printSchedule Function
+//Displays current schedule of appointments
+void schedule::printSchedule() {
+	
+}
