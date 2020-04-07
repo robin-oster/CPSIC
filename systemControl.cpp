970696963c4f4d1d92e2bd749c58c162eb7bb8b3
Matthew Oster <mostost96@gmail.com>
2020-04-07 12:24:38 -0700

    Update and rename systemAccess.cpp to systemControl.cpp

---
+++ systemControl.cpp
@@ -0,0 +1,20 @@
+/*
+	systemAccess.cpp
+	Declaration file for the class systemAccess.
+
+*/
+
+#include "systemControl.h"
+#include "ksuPatient.h"
+#include <string>
+
+bool systemControl::logOn(int id, std::string password) {
+
+}
+
+void systemControl::networkUpdate() {
+
+}
+
+bool systemControl::checkID(patient ksuPatient){
+}
