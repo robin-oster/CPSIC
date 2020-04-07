fc683b13c130cb151ce93de54f41f44ad7d3f8bf
Matthew Oster <mostost96@gmail.com>
2020-04-07 12:30:46 -0700

    Create ksuSystemAccess.cpp

---
+++ ksuSystemAccess.cpp
@@ -0,0 +1,19 @@
+/*
+        ksuSystemAccess.cpp
+        Declaration file for class ksuSystemAccess.
+*/
+
+#include "ksuSystemAccess.h"
+
+/**
+        @author Matthew Oster moster@kent.edu
+*/
+
+void ksuSystemAccess::generateAlert(){
+}
+
+void ksuSystemAccess::notifyKSU(){
+}
+
+void ksuSystemAccess::notifyFlashAlerts(){
+}
